6c4fd66734d636779b365d8c959a579391bcee79
Goose <niken54@mail.ru>
2021-03-07 23:55:05 +0300

    Added rectangle with EBO

--- OpenGLLearn/Main.cpp
+++ OpenGLLearn/Main.cpp
@@ -67,8 +67,14 @@ int main()
 	//Triangle
 	GLfloat vertices[] = {
 			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+			-0.5f,  0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f
+	};
+
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
 
@@ -134,16 +140,24 @@ int main()
 	//VAO
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindVertexArray(0);
+
+	//EBO
+	
 
 	//main cycle
 	while (!glfwWindowShouldClose(window))
@@ -154,7 +168,9 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
