0ab5a14b403d07892114128c89f204ece131f23b
Goose <niken54@mail.ru>
2021-03-30 13:27:58 +0300

    Added Camera class

---
+++ OpenGLLearn/Camera.h
@@ -0,0 +1,72 @@
+#pragma once
+
+//#include <vector>
+
+#include <GL/glew.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+
+enum CameraMovement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+const GLfloat YAW = -90.0f;
+const GLfloat PITCH = 0.0f;
+const GLfloat SPEED = 3.0f;
+const GLfloat SENSITIVITY = 0.25f;
+const GLfloat ZOOM = 45.0f;
+
+class Camera
+{
+public:
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	GLfloat Yaw;
+	GLfloat Pitch;
+
+	GLfloat MovementSpeed;
+	GLfloat MouseSensitivity;
+	GLfloat Zoom;
+
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+		   glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+		   GLfloat yaw = YAW, 
+		   GLfloat pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		this->Position = position;
+		this->Up = up;
+		this->Yaw = yaw;
+		this->Pitch = pitch;
+		this->updateCameraVectors();
+	}
+
+	Camera(GLfloat posX, GLfloat posY, GLfloat posZ,
+		   GLfloat upX,  GLfloat upY,  GLfloat upZ,
+		GLfloat yaw, GLfloat pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		this->Position = glm::vec3(posX, posY, posZ);
+		this->Up = glm::vec3(upX, upY, upZ);
+		this->Yaw = yaw;
+		this->Pitch = pitch;
+		this->updateCameraVectors();
+	}
+
+	glm::mat4 GetViewMatrix();
+
+	void ProcessKeyboard(CameraMovement direction, GLfloat deltaTime);
+
+	void ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset, GLboolean constrainPitch = true);
+
+	void ProcessMouseScroll(GLfloat yoffset);
+
+private:
+	void updateCameraVectors();
+};
+
